64536166778f586a08ecdb7f5fcef0c547290b2e
nkunic <45877400+nkunic@users.noreply.github.com>
2023-08-30 17:01:58 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { WrapperComponent } from './wrapper/wrapper.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, WrapperComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
